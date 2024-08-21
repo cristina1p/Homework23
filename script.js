58bf4c5e7d8fa029fad8173b6a40db28a68cfe23
cristina1p <c1plamadeala@gmail.com>
2024-08-21 13:23:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,211 @@
+
+
+// Exercitiul 1
+
+// let delfini
+// let koalas
+
+// let averageDelfini = (96 + 108 + 89) / 3;
+// let averageKoalas = (88 + 91 + 110) / 3;
+
+// console.log(averageDelfini);
+
+// if (averageDelfini > averageKoalas) {
+//     console.log('Delfinii au castigat!')
+// } else if (averageDelfini < averageKoalas) {
+//     console.log('Koalas au castigat!')
+// } else {
+//     console.log('Remiza')
+// }
+
+//Exercitiul 1, part 3
+
+// let delfini
+// let koalas
+
+// let averageDelfini = (97 + 112 + 101) / 3;
+// let averageKoalas = (109 + 95 + 123) / 3;
+
+// console.log(averageDelfini);
+
+// if (averageDelfini > averageKoalas && averageDelfini > 100) {
+//     console.log('Delfinii au castigat!')
+// } else if (averageDelfini < averageKoalas && averageKoalas > 100) {
+//     console.log('Koalas au castigat!')
+// } else {
+//     console.log('Remiza')
+// }
+
+//Exercitiul 1, part 4
+
+// let delfini
+// let koalas
+
+// let averageDelfini = (97 + 112 + 101) / 3;
+// let averageKoalas = (97 + 112 + 101) / 3;
+
+// console.log(averageDelfini);
+
+// if (averageDelfini > averageKoalas && averageDelfini > 100) {
+//     console.log('Delfinii au castigat!')
+// } else if (averageDelfini < averageKoalas && averageKoalas > 100) {
+//     console.log('Koalas au castigat!')
+// } else if (averageDelfini >= 100 && averageKoalas >= 100 && averageKoalas == averageDelfini) {
+//     console.log('Remiza')
+// } else {
+//     console.log('Nici o echipa nu a castigat trofeul.')
+// }
+
+
+// Exercitiul 2
+
+// if else method
+
+// let bill = 430;
+// let totalBill;
+// let tip15 = bill * 0.15;
+// let tip20 = bill * 0.2;
+
+
+
+// if (bill > 50 && bill < 300) {
+//     totalBill = bill + tip15;
+//     console.log(`The bill was: ${bill}, the tip was: ${tip15}, and the totalBill is: ${totalBill}.`)
+// } else {
+//     totalBill = bill + tip20;
+//     console.log(`The bill was: ${bill}, the tip was: ${tip20}, and the totalBill is: ${totalBill}.`)
+// }
+
+
+
+// ternary operator method
+
+// let bill = 400;  // Value of the bill
+// let tip = (bill > 50 && bill < 300) ? bill * 0.15 : bill * 0.2;  // Calculate tip based on bill amount
+// let totalBill = bill + tip;  // Calculate the total bill including the tip
+
+// console.log(`The bill was: ${bill}, the tip was: ${tip}, and the totalBill is: ${totalBill}.`);
+
+
+
+
+// Switch statement ex
+
+
+// Exercitiul 1
+
+// let day = 1;
+
+// switch (day) {
+//     case 1:
+//         console.log('Luni');
+//         break;
+//     case 2:
+//         console.log('Marti');
+//         break;
+//     case 3:
+//         console.log('Miercuri');
+//         break;
+//     case 4:
+//         console.log('Joi');
+//         break;
+//     case 5:
+//         console.log('Vineri');
+//         break;
+//     case 6:
+//         console.log('Sambata');
+//         break;
+//     case 7:
+//         console.log('Sambata');
+//         break;
+// }
+
+
+// Exercitiul 2
+
+
+// let grade = 4;
+
+// switch (true) {
+//     case (grade >= 1 && grade <= 3):
+//         console.log("Poor");
+//         break;
+//     case (grade >= 4 && grade <= 5):
+//         console.log("Satisfactory");
+//         break;
+//     case (grade >= 6 && grade <= 7):
+//         console.log("Good");
+//         break;
+//     case (grade >= 8 && grade <= 9):
+//         console.log("Very Good");
+//         break;
+//     case (grade == 10):
+//         console.log("Excellent");
+//         break;
+//     default:
+//         console.log("Nota invalida!");
+//       } 
+
+// switch can't compare variable directly to the value of each case, it's better to use if/else
+
+
+
+// Exercitiul 3 
+
+// let color = 'bluE';
+
+// let normalColor = color.toLowerCase();
+
+// switch (normalColor) {
+//     case 'red':
+//         console.log("Stop");
+//         break;
+//     case 'yellow':
+//         console.log("Get Ready");
+//         break;
+//     case 'green':
+//         console.log("Go");
+//         break;
+//     default:
+//         console.log("No color available!")
+// }
+
+
+
+
+// Exercitiul 4
+
+// let seaseon = 'Summer';
+
+// let normalSeason = seaseon.toLowerCase();
+
+// switch (normalSeason) {
+//     case 'winter':
+//         console.log('December, January, February');
+//         break;
+//     case 'spring':
+//         console.log('March, April, May');
+//         break;
+//     case 'summer':
+//         console.log('June, July, August');
+//         break;
+//     case 'fall':
+//         console.log('September, October, November');
+//         break;
+//     default:
+//         console.log('Invalid season!');
+//         break;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
